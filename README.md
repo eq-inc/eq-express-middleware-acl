96eccf9c1cb4e3f6d0a8daa4b5d99c2772c26717
Kazuma Fujimaki <fujimaki@eq-inc.jp>
2017-02-27 15:46:44 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,108 @@
 # eq-express-middleware-acl
+
 Access control middleware for Express
+
+## Class: ExpressMiddlewareACL
+### ExpressMiddlewareACL(middleware)
+- ```middleware``` <Array>    Array of middleware function
+
+#### Example
+```javascript
+function middleware(req, res, next) {
+    next();
+}
+
+const express_middleware_acl = require('eq-express-middleware-acl'),
+    acl = express_middleware_acl([middleware]);
+```
+
+
+### Class Method: ExpressMiddlewareACL.set(middleware)
+Set middleware list to ```middleware``` (Overwrite current list)
+- ```middleware``` <Array>    Array of middleware function
+
+#### Example
+```javascript
+function middleware(req, res, next) {
+    next();
+}
+
+const express_middleware_acl = require('eq-express-middleware-acl'),
+    acl = express_middleware_acl();
+acl.set([middleware]); // Set middleware list to [middleware]
+acl.set([]); // Clear middleware list
+```
+
+
+### Class Method: ExpressMiddlewareACL.add(middleware)
+Add ```middleware``` to end of middleware list
+- ```middleware``` <Array>    Array of middleware function
+
+#### Example
+```javascript
+function middleware(req, res, next) {
+    next();
+}
+
+const express_middleware_acl = require('eq-express-middleware-acl'),
+    acl = express_middleware_acl();
+acl.add([middleware]);
+```
+
+
+### Class Method: ExpressMiddlewareACL.remove(index)
+Remove middleware by index
+- ```index``` <Number>    Index of middleware
+
+#### Example
+```javascript
+function middleware(req, res, next) {
+    next();
+}
+
+const express_middleware_acl = require('eq-express-middleware-acl'),
+    acl = express_middleware([middleware, middleware, middleware]);
+acl.remove(1);
+```
+
+
+### Class Method: middlware
+Get access control middleware function
+
+#### Example
+```javascript
+function middleware(req, res, next) {
+    next();
+}
+
+const express = require('express'),
+    express_middleware_acl = require('eq-express-middleware-acl'),
+    app = express(),
+    acl = express_middleware_acl([middleware]);
+app.use(acl.middleware());
+```
+
+
+## Creating access control middleware
+### Call next access control middleware
+```javascript
+function (req, res, next) {
+    // Call next access control middleware
+    next();
+}
+```
+
+### Allow access and skip next middleware
+```javascript
+function (req, res, next) {
+    // Skip next access control middleware
+    next('route');
+}
+```
+
+### Deny access
+```javascript
+function (req, res, next) {
+    next(new Error('Forbidden'));
+}
+```
---
+++ test/express-middleware-acl.js
@@ -0,0 +1,210 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/* eslint-env es6, mocha, node */
+/* eslint-extends: eslint:recommended */
+'use strict';
+
+
+
+// Variables
+const expect = require('expect.js'),
+    express_middleware_acl = require('../');
+
+
+// Middleware
+function allow(req, res, next) {
+    next('route');
+}
+
+function pass(req, res, next) {
+    next();
+}
+
+function deny(req, res, next) {
+    next(new Error('Forbidden'));
+}
+
+
+// Test
+describe('express-middleware-acl', function () {
+    describe('constructor', function () {
+        describe('Should set intance', function () {
+            it('With middleware', function () {
+                const instance = express_middleware_acl(allow);
+
+                expect(instance.constructor.name).to.be('ExpressMiddlewareACL');
+                expect(instance.list).to.have.length(1);
+                expect(instance.options).to.eql({});
+            });
+
+            it('Without middleware', function () {
+                const instance = express_middleware_acl();
+
+                expect(instance.constructor.name).to.be('ExpressMiddlewareACL');
+                expect(instance.options).to.eql({});
+            });
+
+            it('Without options', function () {
+                const options = {test: true},
+                    instance = express_middleware_acl(null, options);
+
+                expect(instance.constructor.name).to.be('ExpressMiddlewareACL');
+                expect(instance.options).to.eql(options);
+            });
+        });
+    });
+
+    describe('set', function () {
+        describe('Should set middleware', function () {
+            it('Single', function () {
+                const instance = express_middleware_acl([allow]);
+
+                expect(instance.list).to.have.length(1);
+                expect(instance.list.every(function (item) {
+                    return ('allow' === item.name);
+                })).to.be(true);
+                instance.set(deny);
+                expect(instance.list).to.have.length(1);
+                expect(instance.list.every(function (item) {
+                    return ('deny' === item.name);
+                })).to.be(true);
+            });
+
+            it('Multi', function () {
+                const instance = express_middleware_acl([allow]);
+
+                expect(instance.list).to.have.length(1);
+                expect(instance.list.every(function (item) {
+                    return ('allow' === item.name);
+                })).to.be(true);
+                instance.set([deny, deny, deny]);
+                expect(instance.list).to.have.length(3);
+                expect(instance.list.every(function (item) {
+                    return ('deny' === item.name);
+                })).to.be(true);
+            });
+        });
+
+        it('Should clear middleware', function () {
+            const instance = express_middleware_acl([allow, allow, allow, allow, allow]);
+
+            expect(instance.list).to.have.length(5);
+            instance.set();
+            expect(instance.list).to.have.length(0);
+        });
+    });
+
+    describe('add', function () {
+        describe('Should add middleware', function () {
+            it('Single', function () {
+                const instance = express_middleware_acl();
+
+                expect(instance.list).to.have.length(0);
+                instance.add(allow);
+                expect(instance.list).to.have.length(1);
+            });
+
+            it('Multi', function () {
+                const instance = express_middleware_acl();
+
+                expect(instance.list).to.have.length(0);
+                instance.add([allow, deny]);
+                expect(instance.list).to.have.length(2);
+            });
+        });
+    });
+
+    describe('remove', function () {
+        describe('Should remove middleware', function () {
+            it('Single', function () {
+                const instance = express_middleware_acl([allow, allow, deny, allow, allow]);
+                instance.remove(2);
+
+                expect(instance.list).to.have.length(4);
+                expect(instance.list.every(function (item) {
+                    return ('allow' === item.name);
+                })).to.be(true);
+            });
+
+            it('Multi', function () {
+                const instance = express_middleware_acl([allow, allow, deny, deny, allow]);
+                instance.remove(2, 2);
+
+                expect(instance.list).to.have.length(3);
+                expect(instance.list.every(function (item) {
+                    return ('allow' === item.name);
+                })).to.be(true);
+            });
+        });
+    });
+
+    describe('middleware', function () {
+        it('Should get middleware function', function () {
+            const instance = express_middleware_acl(),
+                middlware = instance.middleware();
+
+            expect(middlware).to.be.a(Function);
+        });
+
+        it('Should skip acl', function (done) {
+            const instance = express_middleware_acl(),
+                middleware = instance.middleware();
+
+            middleware({}, {}, function (result) {
+                expect(result).to.be(undefined);
+
+                done();
+            });
+        });
+
+        it('Should deny request', function (done) {
+            const instance = express_middleware_acl([pass, pass, pass, deny, allow]),
+                middleware = instance.middleware();
+
+            middleware({}, {}, function (result) {
+                expect(result.message).to.be('Forbidden');
+
+                done();
+            });
+        });
+
+        it('Should allow request', function (done) {
+            const instance = express_middleware_acl([pass, pass, pass, allow, deny]),
+                middleware = instance.middleware();
+
+            middleware({}, {}, function (result) {
+                expect(result).to.be(undefined);
+
+                done();
+            });
+        });
+
+        it('Should pass all middleware', function (done) {
+            let count = 0;
+            const test = function (req, res, next) {
+                    count = count + 1;
+
+                    next();
+                },
+                instance = express_middleware_acl([test, test, test, test, test]),
+                middleware = instance.middleware();
+
+            middleware({}, {}, function (result) {
+                expect(count).to.be(5);
+                expect(result).to.be(undefined);
+
+                done();
+            });
+        });
+    });
+});
+
+
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
